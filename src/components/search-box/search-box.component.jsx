96c8eb48002dc46047c78a398fd33f878f3cdfa0
Kundan choudhary <zrkundan@gmail.com>
2021-06-12 08:20:36 +0530

    first commit

---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,7 @@
+import './search-box.styles.css'
+
+export const SearchBox = ({ placeholder, handleChange }) => {
+    return(
+        <input type="search" className="search" placeholder={ placeholder } onChange={ handleChange }/> 
+    )
+}
